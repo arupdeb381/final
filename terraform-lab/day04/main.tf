da8530226d616382414e91294710eb05f577107b
Arup <arup.deb381@gmail.com>
2026-08-19 11:01:22 +0530

    updated files

---
+++ terraform-lab/day04/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "s3" {
+    bucket = "tfbucket381"
+    key    = "Dev/tf.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+#Create S3 bucket 
+resource "aws_s3_bucket" "example" {
+  bucket = "arupdeb831-day04"
+
+  tags = {
+    Name        = "day04"
+    Environment = "Dev"
+  }
+}
