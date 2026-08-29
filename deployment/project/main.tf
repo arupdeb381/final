3e87260bf785135257ed41821148bc4fac662966
Arup <arup.deb381@gmail.com>
2026-08-29 23:44:27 +0530

    29-08-2026

---
+++ deployment/project/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+#Create S3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "arupdeb831-1"
+
+  tags = {
+    Name        = "day03"
+    Environment = "Dev"
+  }
+}
---
+++ k8s/Abhishek-VM/refresh.sh
@@ -0,0 +1,5 @@
+while true; do
+    clear
+    kubectl get pods,deploy,rs,svc;
+    sleep 1
+done
