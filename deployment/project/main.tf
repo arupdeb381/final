8b5cf8f2ec60600c7027e23439cbb133cf3b9ed0
Arup <arup.deb381@gmail.com>
2026-08-30 01:19:23 +0530

    Rename VPC

--- deployment/project/main.tf
+++ deployment/project/main.tf
@@ -21,6 +21,11 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "vpc01" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    "name" = "vpc01"
+    "Environment" = "Dev"
+  }
+
 }
 # Create Public subnet 
 resource "aws_subnet" "public-snet" {
